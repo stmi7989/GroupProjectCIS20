87fdce003aeb1d1f57db0e8486bc79a50ee4a29d
stmi7989 <stmi7989@cc.peralta.edu>
2017-11-06 14:19:02 -0800

    Update TestScoreEval.asm

--- TestScoreEval.asm
+++ TestScoreEval.asm
@@ -1,66 +1,94 @@
 TITLE CalcGrade								      (CalcGrade.asm)
-; Creation Date : 04/17/15
+; Names : Inderpal Virdi, Padraic Reilly, Obaid Hasseeb, Stephen Miller
+; Creation Date : 11/15/17
 ; Revisions : 1
-; Date : 04/17/15
+; Date : 11/06/17
 ; Program Description :
-	; Create a procedure named CalcGrade that receives an integer value between 0 and 100, and returns  single capital letter in the AL register. Preserve all other values between calls to the procedure. The letter returned by the procedure should be according to the following:
+	; Create a procedure named CalcGrade that receives an integer value between 0 and 100, and returns  single capital letter in the AL register. 
+	; Preserve all other values between calls to the procedure. The letter returned by the procedure should be according to the following:
 	; Score 90 to 100 Grade A
 	; Score 80 to 89 Grade B
 	; Score 70 to 79 Grade C
 	; Score 60 to 69 Grade D
 	; Score 0 to 59 Grade F
-	; Write test program that generates 10 random integers between 50 and 100 inclusive. Each time an integer is generated, pass is to the CalcGrade procedure.
+	; Write test program that generates 10 random integers between 50 and 100 inclusive. 
+	; Each time an integer is generated, pass is to the CalcGrade procedure. You can test your program using a debugger,
+    ; or if you prefer to use the book's library, you can display each integer and its corresponding letter grade.
 
 INCLUDE Irvine32.inc
     
-	.data
+.data
+	newString BYTE "Your grade is ",0
 	grade BYTE ?,0
-	grA BYTE "A",0
-	grB BYTE "B",0
-	grC BYTE "C",0
-	grD BYTE "D",0
-	grF BYTE "F",0
 	out_of_range BYTE "The integer is not <= 100 and >= 0",0
 	
 	.code
 main PROC
 	call Randomize
 	mov ecx,10
-L1: 
+again: 
 	mov eax,51
 	call RandomRange
 	add eax,50
 	call GradeCalc
 	mov edx,0
+	mov edx, OFFSET newString
+	call WriteString
 	mov grade,al
 	mov edx, OFFSET grade
 	call WriteString
 	call Crlf
-	loop L1
+	loop again
     exit
 main ENDP
 
 GradeCalc PROC
-	.IF (eax <= 100 && eax >= 0)
-		.IF (eax <= 100) && (eax > 89)
-			mov al,grA
-		.ELSEIF (eax < 90) && (eax > 79)
-			mov al,grB
-		.ELSEIF (eax < 80) && (eax > 69)
-			mov al,grC
-		.ELSEIF (eax < 70) && (eax > 59)
-			mov al,grD
-		.ELSE
-			mov al,grF
-		.ENDIF
+OutofRangeHigh:
+	cmp	eax,101
+	jb  Grade_A
+	mov edx, OFFSET out_of_range
+	call WriteString
+	call Crlf
+	call WriteDec
+	jmp finished
 
-	.ELSE
-		mov edx,OFFSET out_of_range
-		call WriteString
-		call Crlf
-		ret
-	.ENDIF
+Grade_A:
+	cmp	eax,90
+	jb	Grade_B
+	mov	al,'A'
+	jmp	finished
 
+Grade_B:
+	cmp	eax,80
+	jb	Grade_C
+	mov	al,'B'
+	jmp	finished
+	
+Grade_C:
+	cmp	eax,70
+	jb	Grade_D
+	mov	al,'C'
+	jmp	finished
+
+Grade_D:
+	cmp	eax,60
+	jb	Grade_F
+	mov	al,'D'
+	jmp	finished
+
+Grade_F:
+	cmp eax,0
+	jb  OutofRangeLow
+	mov	al,'F'
+	jmp finished
+
+OutofRangeLow:
+	mov edx, OFFSET out_of_range
+	call WriteString
+	call Crlf
+	call WriteDec
+	jmp finished
+finished:
 	ret
 
 GradeCalc ENDP
